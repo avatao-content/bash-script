22b3f63f2c1ca85a48a1c09d646dab11ec051088
Gergo Turcsanyi <gergo.turcsanyi@avatao.com>
2021-05-14 11:07:21 +0200

    What's your name?

--- script.sh
+++ script.sh
@@ -5,3 +5,10 @@ echo "Hello $USER"
 echo "Hey i am $USER and will be telling you about the current processes"
 echo "Running processes List"
 ps
+echo "Hey what's Your First Name?"
+read a
+echo "welcome Mr./Mrs. $a, would you like to tell us, Your Last Name"
+read b
+echo "Thanks Mr./Mrs. $a $b for telling us your name"
+echo "*******************"
+echo "Mr./Mrs. $b, it's time to say you good bye"
